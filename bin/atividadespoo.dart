67a617d448625e3cf261a7bd642f56c560c53e59
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-11-26 10:22:34 -0400

    first commit

---
+++ bin/atividadespoo.dart
@@ -0,0 +1,5 @@
+import 'package:atividadespoo/atividadespoo.dart' as atividadespoo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${atividadespoo.calculate()}!');
+}
---
+++ test/atividadespoo_test.dart
@@ -0,0 +1,8 @@
+import 'package:atividadespoo/atividadespoo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
